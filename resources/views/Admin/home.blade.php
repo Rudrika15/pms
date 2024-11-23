267c121d90cde630c079d410f7b60f61bc4545fd
Parvez Memon <137087093+Parvez2003@users.noreply.github.com>
2024-11-23 17:41:21 +0530

    changes

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -7,6 +7,7 @@
                     <span class="page-title-icon bg-gradient-primary text-white me-2">
                         <i class="mdi mdi-home"></i>
                     </span> Dashboard
+                    {{-- code push --}}
                     <a class="btn btn-primary" style="float: right" href="{{ route('viewProject') }}">View Project</a>
 
                 </h3>
