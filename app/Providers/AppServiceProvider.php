bcb5207312b2f4fc1f973b73069df591d0952573
Parvez Memon <137087093+Parvez2003@users.noreply.github.com>
2024-11-26 14:56:00 +0530

    middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Http\Middleware\CheckRole;
 use Illuminate\Support\Facades\View;
 use App\Models\PmsProject;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         View::share('projects', PmsProject::all());
+        Route::aliasMiddleware('role.notuser', \App\Http\Middleware\CheckRole::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,19 +20,21 @@ Route::middleware(['auth'])->group(function () {
 
     // Project Crud
     Route::get('projects', [PmsProjectController::class, 'index'])->name('projects.index');
-    Route::get('project/create', [PmsProjectController::class, 'create'])->name('project.create');
+    Route::get('project/create', [PmsProjectController::class, 'create'])->name('project.create')->middleware('role.notuser');
     Route::post('projects', [PmsProjectController::class, 'store'])->name('project.store');
     Route::get('projects/{id}/edit', [PmsProjectController::class, 'edit'])->name('project.edit');
     Route::post('projects/{id}', [PmsProjectController::class, 'update'])->name('project.update');
     Route::get('projects/{id}', [PmsProjectController::class, 'destroy'])->name('project.destroy');
-
-    //teams crud
-    Route::get('teams', [PmsTeamController::class, 'index'])->name('teams.index');
-    Route::get('teams/create', [PmsTeamController::class, 'create'])->name('teams.create');
-    Route::post('teams', [PmsTeamController::class, 'store'])->name('teams.store');
-    Route::get('teams/{id}/edit', [PmsTeamController::class, 'edit'])->name('teams.edit');
-    Route::put('teams/{id}', [PmsTeamController::class, 'update'])->name('teams.update');
-    Route::get('teams/{id}', [PmsTeamController::class, 'destroy'])->name('teams.destroy');
+    Route::middleware('role.notuser') // Apply the middleware to all the following routes
+        ->group(function () {
+            //teams crud
+            Route::get('teams', [PmsTeamController::class, 'index'])->name('teams.index');
+            Route::get('teams/create', [PmsTeamController::class, 'create'])->name('teams.create');
+            Route::post('teams', [PmsTeamController::class, 'store'])->name('teams.store');
+            Route::get('teams/{id}/edit', [PmsTeamController::class, 'edit'])->name('teams.edit');
+            Route::put('teams/{id}', [PmsTeamController::class, 'update'])->name('teams.update');
+            Route::get('teams/{id}', [PmsTeamController::class, 'destroy'])->name('teams.destroy');
+        });
 
     //task crud
     Route::get('tasks/{id?}', [PmsTaskController::class, 'index'])->name('tasks.index');
