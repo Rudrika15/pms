8fcdce7404e02b3ed69380c3450cca545c7d874b
Rudrika15 <rudrikadave20226@gmail.com>
2024-11-26 13:14:37 +0530

    merge code

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -68,7 +68,7 @@
                     </div>
                 @endrole
 
-                <div class="col-md-4 stretch-card grid-margin">
+                {{-- <div class="col-md-4 stretch-card grid-margin">
                     <div class="card bg-gradient-success card-img-holder text-white">
                         <div class="card-body">
                             <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
@@ -78,7 +78,7 @@
                             <h6 class="card-text">Increased by 5%</h6>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
             <div class="row">
                 <div class="col-md-7 grid-margin stretch-card">
