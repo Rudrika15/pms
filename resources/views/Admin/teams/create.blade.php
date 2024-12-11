fe0216c5627f5ef4ac47b9979a57b9ee6dd8f56b
Parvez Memon <137087093+Parvez2003@users.noreply.github.com>
2024-12-11 19:35:48 +0530

    changes

--- resources/views/Admin/teams/create.blade.php
+++ resources/views/Admin/teams/create.blade.php
@@ -11,31 +11,35 @@
                 <select name="project_id" class="form-select" id="">
                     <option value="" selected disabled>Select Project</option>
                     @foreach ($projects as $item)
-                        <option value="{{ $item->id }}" @if (url()->previous() == route('tasks.create', $item->id)) selected @endif disabled>
-                            {{ $item->title }}
-                        </option>
-                    @endforeach
-                </select>
-                @error('project_id')
-                    <span class="text-danger">Please Select Project</span>
+                        @if (url()->previous() == route('tasks.create', $item->id))
+                            <option value="{{ $item->id }}" selected>{{ $item->title }}</option>
+                        @break
+
+                    @else
+                        <option value="{{ $item->id }}">{{ $item->title }}</option>
+                    @endif
+                @endforeach
+            </select>
+            @error('project_id')
+                <span class="text-danger">Please Select Project</span>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <div class="row">
+                @foreach ($users as $item)
+                    <div class="col-md-2 pt-2">
+                        <label for="{{ $item->id }}">
+                            <input type="checkbox" id="{{ $item->id }}" class="" name="user_id[]"
+                                value="{{ $item->id }}">{{ explode(' ', $item->name)[0] }}</input>
+                        </label>
+                    </div>
+                @endforeach
+                @error('user_id')
+                    <span class="text-danger">Please Select User</span>
                 @enderror
             </div>
-            <div class="mb-3">
-                <div class="row">
-                    @foreach ($users as $item)
-                        <div class="col-md-2 pt-2">
-                            <label for="{{ $item->id }}">
-                                <input type="checkbox" id="{{ $item->id }}" class="" name="user_id[]"
-                                    value="{{ $item->id }}">{{ explode(' ', $item->name)[0] }}</input>
-                            </label>
-                        </div>
-                    @endforeach
-                    @error('user_id')
-                        <span class="text-danger">Please Select User</span>
-                    @enderror
-                </div>
-            </div>
-            <button class="btn btn-primary" type="submit">Create Team</button>
-        </form>
-    </div>
+        </div>
+        <button class="btn btn-primary" type="submit">Create Team</button>
+    </form>
+</div>
 @endsection
